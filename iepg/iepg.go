8bc6f49a5cad5e2a8aff46948358645b47e780db
Yuya Adachi <moguriso@tizen.moe>
2020-01-01 04:25:42 +0900

    月日の経過で修正が不要になったので戻す(2020年末に再発するので年内に直す...)

--- iepg/iepg.go
+++ iepg/iepg.go
@@ -59,12 +59,7 @@ func PrintReserve(dp *p.DynamicParam) {
 			fmt.Println("Content-type: application/x-tv-program-info; charset=shift_jis")
 			fmt.Println("version: 1")
 			fmt.Println("station: " + convertStation(v.Station))
-			/* TODO: 格好悪いので後で直す...が、そもそも年が入ってないほうが悪い... */
-			if v.Month < 12 {
-				fmt.Println("year: " + fmt.Sprintf("%04d", time.Now().Year()))
-			} else {
-				fmt.Println("year: " + fmt.Sprintf("%04d", time.Now().Year()+1))
-			}
+			fmt.Println("year: " + fmt.Sprintf("%04d", time.Now().Year()))
 			fmt.Println("month: " + fmt.Sprintf("%02d", v.Month))
 			fmt.Println("date: " + fmt.Sprintf("%02d", v.Date))
 			fmt.Println("start: " + fmt.Sprintf("%02d:%02d", v.Start_h, v.Start_m))
@@ -185,12 +180,7 @@ func OutputIepg(fileName string, in *pl.ReadData) {
 	_, err = sjisWriter.WriteString("Content-type: application/x-tv-program-info; charset=shift_jis\n")
 	_, err = sjisWriter.WriteString("version: 1\n")
 	_, err = sjisWriter.WriteString("station: " + convertStation(in.Station) + "\n")
-	/* TODO: 格好悪いので後で直す...が、そもそも年が入ってないほうが悪い... */
-	if in.Month < 12 {
-		_, err = sjisWriter.WriteString("year: " + fmt.Sprintf("%04d", time.Now().Year()+1) + "\n")
-	} else {
-		_, err = sjisWriter.WriteString("year: " + fmt.Sprintf("%04d", time.Now().Year()) + "\n")
-	}
+	_, err = sjisWriter.WriteString("year: " + fmt.Sprintf("%04d", time.Now().Year()) + "\n")
 	_, err = sjisWriter.WriteString("month: " + fmt.Sprintf("%02d", in.Month) + "\n")
 	_, err = sjisWriter.WriteString("date: " + fmt.Sprintf("%02d", in.Date) + "\n")
 	_, err = sjisWriter.WriteString("start: " + fmt.Sprintf("%02d:%02d", in.Start_h, in.Start_m) + "\n")
