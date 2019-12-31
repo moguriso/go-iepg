a951c7070303a0412458b5daeffa84856da6b3ac
Yuya Adachi <moguriso@tizen.moe>
2020-01-01 04:18:36 +0900

    引数（-r）で予約ファイル名を指定できるように変更

--- main_windows.go
+++ main_windows.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"go-iepg/iepg"
 	"go-iepg/param"
+	"os"
+)
+
+var (
+	reserveFile = flag.String("r", "", "reserve file name (fullpath)")
 )
 
 func main() {
-	d_conf := param.LoadDynamicParam("reserve.json")
+
+	flag.Parse()
+	if _, err := os.Stat(*reserveFile); err != nil {
+		*reserveFile = "./reserve.json"
+	}
+
+	fmt.Println("file = ", *reserveFile)
+	d_conf := param.LoadDynamicParam(*reserveFile)
 	for _, v := range d_conf {
 		iepg.Reserve(v)
 	}
